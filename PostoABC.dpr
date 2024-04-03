3cea4455a7700e706be56650db2c93f0e5027f54
Antony Moraes <antonylmtec2016@gmail.com>
2024-04-03 18:46:37 -0300

    ajuste

--- PostoABC.dpr
+++ PostoABC.dpr
@@ -4,7 +4,9 @@ uses
   Vcl.Forms,
   UfrmPrincipal in 'MVC\View\UfrmPrincipal.pas' {FrmPrincipal},
   TTanque in 'MVC\Classes\TTanque.pas',
-  UBomba in 'MVC\Classes\UBomba.pas';
+  UBomba in 'MVC\Classes\UBomba.pas',
+  AbastecimentoModel in 'MVC\Model\AbastecimentoModel.pas',
+  Abastecimento in 'MVC\Classes\Abastecimento.pas';
 
 {$R *.res}
 
